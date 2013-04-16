c8ec46e6389a8f3fc7972888eafa2edfa9043e9a
artemp <artem@mapnik.org>
2013-04-16 14:56:47 +0100

    + add reading from filesystem tests

--- main.cpp
+++ main.cpp
@@ -18,14 +18,58 @@ int main(int argc, char** argv)
     }
     unsigned NUM_RUNS = std::atoi(argv[2]);
     std::string filename(argv[1]);
-    std::ifstream is(filename.c_str() , std::ios::binary);
-    std::string buffer((std::istreambuf_iterator<char>(is)),
-                       std::istreambuf_iterator<char>());
 
 
     // node-blend
     {
-        std::cerr << "========== Node-blend ImageReader" << std::endl;
+        std::cerr << "========== Node-blend ImageReader FROM FILE" << std::endl;
+        std::cout << "NUM_RUNS="<<NUM_RUNS << std::endl;
+        boost::timer::auto_cpu_timer t;
+        for (unsigned count=0; count < NUM_RUNS; ++count)
+        {
+            std::ifstream is(filename.c_str() , std::ios::binary);
+            std::string buffer((std::istreambuf_iterator<char>(is)),
+                               std::istreambuf_iterator<char>());
+            const std::unique_ptr<ImageReader> layer(ImageReader::create((uint8_t*)buffer.c_str(),buffer.size()));
+            layer->decode();
+        }
+    }
+
+
+    { // mapnik
+        std::cerr << "========== Mapnik image_reader FROM FILE" << std::endl;
+        try {
+            std::cout << "NUM_RUNS="<<NUM_RUNS << std::endl;
+            boost::timer::auto_cpu_timer t;
+            for (unsigned count=0; count < NUM_RUNS; ++count)
+            {
+                const std::unique_ptr<mapnik::image_reader> reader(mapnik::get_image_reader(filename));
+                if (reader.get())
+                {
+                    unsigned width = reader->width();
+                    unsigned height = reader->height();
+                    mapnik::image_data_32 buffer(width,height);
+                    reader->read(0,0,buffer);
+                }
+                else
+                {
+                    std::cerr << "Can't create reader ...:)" << std::endl;
+                }
+            }
+        }
+        catch (mapnik::image_reader_exception const& ex)
+        {
+            std::cerr << ex.what() << std::endl;
+        }
+    }
+
+    std::ifstream is(filename.c_str() , std::ios::binary);
+    std::string buffer((std::istreambuf_iterator<char>(is)),
+                       std::istreambuf_iterator<char>());
+
+    // node-blend
+    {
+        std::cerr << "========== Node-blend ImageReader FROM MEM BUFFER" << std::endl;
         std::cout << "NUM_RUNS="<<NUM_RUNS << std::endl;
         boost::timer::auto_cpu_timer t;
         for (unsigned count=0; count < NUM_RUNS; ++count)
@@ -37,13 +81,13 @@ int main(int argc, char** argv)
 
 
     { // mapnik
-        std::cerr << "========== Mapnik image_reader" << std::endl;
+        std::cerr << "========== Mapnik image_reader FROM MEM BUFFER" << std::endl;
         try {
             std::cout << "NUM_RUNS="<<NUM_RUNS << std::endl;
             boost::timer::auto_cpu_timer t;
             for (unsigned count=0; count < NUM_RUNS; ++count)
             {
-                const std::unique_ptr<mapnik::image_reader> reader(mapnik::get_image_reader((char*)buffer.c_str(),buffer.size()));
+                const std::unique_ptr<mapnik::image_reader> reader(mapnik::get_image_reader(buffer.data(), buffer.size()));
                 if (reader.get())
                 {
                     unsigned width = reader->width();
